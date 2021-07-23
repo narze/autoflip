86784e67db2d9636aca957d49cde6064660ad29f
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2021-07-23 12:00:11 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,73 @@
+import { RefreshingAuthProvider } from "@twurple/auth"
+import { ChatClient } from "@twurple/chat"
+import { promises as fs } from "fs"
+import dotenv from "dotenv-flow"
+
+dotenv.config({
+  default_node_env: "development",
+})
+
+const gachaIntervalMs = 180 * 1000
+const stopTime = 2 * 60 * 60 * 1000
+let running = true
+
+const clientId = process.env.CLIENT_ID as string
+const clientSecret = process.env.CLIENT_SECRET as string
+const channel = process.env.CHANNEL as string
+
+async function main() {
+  setTimeout(() => {
+    console.log("Stopped")
+    running = false
+  }, stopTime)
+
+  const tokenData = JSON.parse(await fs.readFile("./tokens.json", "utf-8"))
+  const auth = new RefreshingAuthProvider(
+    {
+      clientId,
+      clientSecret,
+      onRefresh: async (newTokenData) =>
+        await fs.writeFile(
+          "./tokens.json",
+          JSON.stringify(newTokenData, null, 2),
+          "utf-8"
+        ),
+    },
+    tokenData
+  )
+
+  const chatClient = new ChatClient(auth, { channels: [channel] })
+
+  let amount: number | string = 1
+
+  chatClient.onConnect(async () => {
+    console.log("CONNECTED", { channel })
+
+    setInterval(async () => {
+      if (!running) {
+        return
+      }
+
+      const message = `!gacha ${amount}`
+      await chatClient.say(channel, message).then(
+        () => {
+          console.log("Sent", { message })
+        },
+        (reason) => {
+          console.error("Not sent", { reason })
+        }
+      )
+
+      // Swap amount to prevent duplicated message
+      if (amount == 1) {
+        amount = "(github.com/narze/autogacha)"
+      } else {
+        amount = 1
+      }
+    }, gachaIntervalMs)
+  })
+
+  await chatClient.connect()
+}
+
+main()
